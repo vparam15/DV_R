822a7d7563f4cc3921216a3f92ba5389c1504a93
Varun Parameswaran <vparam15@gmail.com>
2015-09-15 18:45:46 -0500

    knitting html correctly

--- 01 Data/Proj1Script.R
+++ 01 Data/Proj1Script.R
@@ -1,8 +1,20 @@
-require("jsonlite")
-require("RCurl")
+require(dplyr)
+require(extrafont)
+require(ggplot2)
+require(ggthemes)
+require(gplots)
+require(grid)
+require(jsonlite)
+require(lubridate)
+require(RCurl)
+require(reshape2)
+require(rstudioapi)
+require(tableplot)
+require(tidyr)
+require(jsonlite)
+require(RCurl)
 # Change the USER and PASS below to be your UTEid
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_vp5467', PASS='orcl_vp5467', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
 summary(df)
 head(df)
 
